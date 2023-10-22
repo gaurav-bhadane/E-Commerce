9f2e39d2faf9bbfe61ed2035dadb17496b061918
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2023-10-22 18:51:47 +0530

    Product Section Ended

--- main.js
+++ main.js
@@ -35,3 +35,18 @@ window.onscroll =()=>{
     account.classList.remove('active')
     navbar.classList.remove('active')
 }
+var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 4
+      },
+    },
+  });
