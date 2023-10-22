7db996822e59f7f8752006f56f17d1947a4aeb21
gaurav-bhadane <gauravpbhadane2304@gmail.com>
2023-10-22 20:39:30 +0530

    Review Section Completed

--- main.js
+++ main.js
@@ -50,3 +50,18 @@ var swiper = new Swiper(".product-slider", {
       },
     },
   });
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 4
+      },
+    },
+  });
